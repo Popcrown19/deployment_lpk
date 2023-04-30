4627e80842b6a2d80faaba50c57969ce898cf678
DESKTOP-ETCKH38\Meidi <adeliaalmas03@gmail.com>
2023-04-30 13:49:58 +0700

    initial deployment

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Menghitung Volume Sebenarnya pada Perhitungan Alat Gelas Kalibrasi')
+
+st.title('Perhitungan densitas air')
+Suhu_air = st.number_input('Masukkan nilai suhu air')
+
+tombol = st.button('Hitung nilai densitas air')
+
+if tombol:
+    Nilai_densitas_air = 0,999974-((Suhu_air-3,989)^2 * (Suhu_air+338,636))/(563385,4*(Suhu_air+72,45147))
+    st.success(f'Nilai densitas air adalah {Nilai_densitas_air}')
