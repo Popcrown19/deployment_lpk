4ce8ff0c731e887f6c6710f6eeb0709915be073c
Popcrown19 <130158791+Popcrown19@users.noreply.github.com>
2023-05-02 14:45:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import numpy as np
 st.title('Menghitung Volume Sebenarnya pada Perhitungan Alat Gelas Kalibrasi')
 
 st.title('Perhitungan densitas air')
-Suhu_air = st.number_input('Masukkan nilai suhu air')
+Suhu_air = st.number_input('Masukan nilai suhu air')
 
 tombol = st.button('Hitung nilai densitas air')
 
 if tombol:
-    Nilai_densitas_air = 0,999974-((Suhu_air-3,989)^2 * (Suhu_air+338,636))/(563385,4*(Suhu_air+72,45147))
-    st.success(f'Nilai densitas air adalah {Nilai_densitas_air}')
+    Nilai_densitas_air = 0,999974-((Suhu_air-3,989)**2*(Suhu_air+338,636))/(563385,4*(Suhu_air+72,45147))
+    st.success(f'Nilai densitas air adalah (Nilai_densitas_air)')
+
