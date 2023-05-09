bc973b4375c0ee629aa8df80355b3672c8c2e00d
Popcrown19 <130158791+Popcrown19@users.noreply.github.com>
2023-05-09 22:40:27 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -19,16 +19,16 @@ if navbar == 'Kalkulator Kalibrasi' :
     with tab1 :
         st.title('Menghitung Volume Sebenarnya pada Perhitungan Alat Gelas Kalibrasi')
         st.title('Perhitungan densitas air')
-        Suhu_air = st.number_input('Masukan nilai suhu air')
+        Suhu_air = st.number_input('Masukan nilai suhu air', format='%.1f')
         tombol = st.button('Hitung nilai densitas air')
         if tombol:
             Nilai_densitas_air = 0.999974 - (((Suhu_air - 3.989)**2) * (Suhu_air + 338.636)) / (563385.4 * (Suhu_air + 72.45147))
             st.success(f'Nilai densitas air adalah' +str(Nilai_densitas_air))
 
         st.title('Perhitungan densitas udara')
-        Kelembaban_udara = st.number_input('Masukan nilai kelembaban udara')
-        Tekanan_udara = st.number_input('Masukan nilai tekanan udara')
-        Suhu_udara = st.number_input('Masukan nilai suhu udara')
+        Kelembaban_udara = st.number_input('Masukan nilai kelembaban udara', format='%.1f')
+        Tekanan_udara = st.number_input('Masukan nilai tekanan udara', format='%.1f')
+        Suhu_udara = st.number_input('Masukan nilai suhu udara', format='%.1f')
         tombol = st.button('Hitung nilai densitas udara')
         if tombol:
             Nilai_densitas_udara = ((0.464554 * Tekanan_udara) - (Kelembaban_udara * (0.00252 * Suhu_udara - 0.020582)))/((237.15 + Suhu_udara) * 1000)
@@ -38,7 +38,7 @@ if navbar == 'Kalkulator Kalibrasi' :
         st.title('Perhitungan Volume Sebenarnya')
         Massa_air = st.number_input('Masukan nilai massa air', format='%.4f')
         Koefisien_muai_volume = st.number_input('Masukan nilai koefisien muai volume', format='%.5f')
-        Suhu_air = st.number_input('Masukan nilai suhu air',key=1)
+        Suhu_air = st.number_input('Masukan nilai suhu air',format='%.1f')
         Densitas_air = st.number_input('Masukan nilai densitas air')
         Densitas_udara = st.number_input('Masukan nilai densitas udara')
 
