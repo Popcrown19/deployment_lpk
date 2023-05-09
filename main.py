c08f327de6a3b7821dcc6d13f0640fe43b8e3b1c
Popcrown19 <130158791+Popcrown19@users.noreply.github.com>
2023-05-09 22:33:07 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ if navbar == 'Kalkulator Kalibrasi' :
         Massa_air = st.number_input('Masukan nilai massa air', format='%.4f')
         Koefisien_muai_volume = st.number_input('Masukan nilai koefisien muai volume', format='%.5f')
         Suhu_air = st.number_input('Masukan nilai suhu air',key=1)
-        Densitas_air = st.number_input('Masukan nilai densitas air')
-        Densitas_udara = st.number_input('Masukan nilai densitas udara')
+        Densitas_air = Nilai_densitas_air
+        Densitas_udara = Nilai_densitas_udara
 
         tombol = st.button('Hitung nilai volume sebenarnya')
         
