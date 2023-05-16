d5bed08211812af195a896294c909f8f74aeb0ab
Popcrown19 <130158791+Popcrown19@users.noreply.github.com>
2023-05-16 23:15:24 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ if navbar == 'Kalkulator Kalibrasi' :
         tombol = st.button('Kesimpulan akhir')
         
         if tombol:
-            Kesimpulan_akhir = st.write('Didapat nilai Densitas air adalah', Nilai_densitas_airZ, 'nilai Densitas udara adalah', Nilai_densitas_udaraS, 'dan nilai Volume sebenarnya adalah', Nilai_volume_sebenarnyaK)
+            Kesimpulan_akhir = st.write('Didapat nilai Volume sebenarnya adalah', Nilai_volume_sebenarnyaK)
             st.success(f'Kesimpulan akhir adalah' (Kesimpulan_akhir))                   
             
             
