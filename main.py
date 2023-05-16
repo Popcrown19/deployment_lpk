7a507292e178fdca46bc40482f7c8215e0b7419b
Popcrown19 <130158791+Popcrown19@users.noreply.github.com>
2023-05-16 20:02:34 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-navbar = option_menu(menu_title=None, options=['Home','Kalkulator Kalibrasi'],default_index=0, icons=['0', '0'], styles={'nav-link' : {'text-align': 'center'}})
+navbar = option_menu(menu_title=None, options=['Kalibrasi Alat Gelas','Kalkulator Kalibrasi','About Us'],default_index=0, icons=['0', '0','0'], styles={'nav-link' : {'text-align': 'center'}})
 
-# Home #
-if navbar == 'Home':
-    st.title('Kelompok 4')
-    st.write('Nama Anggota :')
-    st.write('Adelia Almas Nurvani - 2220440')
-    st.write('Carolyn El Yire Penna Hutajulu - 2220450')
-    st.write('Muhammad Rafi Fadhlurrohman - 2220470')
-    st.write('Oasima Oktaviani Matondang - 2220480')
-    st.write('Violin Febriani - 2220496')
-
+# Kalibrasi Alat Gelas #
+if navbar == 'Kalibrasi Alat Gelas' :
+    tab1, tab2 = st.tabs(['Penjelasan', 'Rumus'])
+    with tab1 :
+        st.title('Penjelasan')
+        st.write('jgjhgj')
+       
+    with tab2 :
+        st.title('Rumus')
+        st.write('Menghitung Volume Sebenarnya : ')
+        st.write('V20 = m(1 - y(t - 20))/(ρair - ρudara)')
+        st.write('Menghitung Densitas Air : ')
+        st.write('0.999974 - (ta - 3.989)^2 (ta + 338.636)/563385.4(ta + 72.45147)')
+        st.write('Menghitung Densitas Udara : ')
+        st.write('0.464554P - H(0.00252tu - 0.020582)/(237.15 + tu)1000')
+        
+        st.write('Keterangan :')
+        st.write('V20 = Volume sebenarnya (mL)')
+        st.write('m = Massa air (gram)')
+        st.write('ρair = Densitas air (gram/mL)')
+        st.write('ρudara = Densitas udara (gram/mL)')
+        st.write('ta = Suhu air (°C)')
+        st.write('tu = Suhu udara (°C)')
+        st.write('y = Koefisien muai volume (°C^-1)')
+    
 # Kalkulator Kalibrasi #
 if navbar == 'Kalkulator Kalibrasi' :
     tab1, tab2 = st.tabs(['PERHITUNGAN DENSITAS', 'PERHITUNGAN VOLUME SEBENARNYA'])
@@ -50,3 +65,16 @@ if navbar == 'Kalkulator Kalibrasi' :
         if Densitas_air == 0 or Densitas_udara == 0:
                 st.write('ADA DATA YANG BELUM TERISI!')
             
+# Home #
+if navbar == 'About Us':
+   
+    st.title('About Us')
+    st.write('blabkaboalakskds (ISI TEKS NANTI KALIMATNYA)')
+    st.write('Nama Anggota :')
+    st.write('Adelia Almas Nurvani - 2220440')
+    st.write('Carolyn El Yire Penna Hutajulu - 2220450')
+    st.write('Muhammad Rafi Fadhlurrohman - 2220470')
+    st.write('Oasima Oktaviani Matondang - 2220480')
+    st.write('Violin Febriani - 2220496')
+    
+    
