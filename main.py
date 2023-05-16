4ccd5af271ab2d2196f7ab586cc9e5e097a62dbb
Popcrown19 <130158791+Popcrown19@users.noreply.github.com>
2023-05-16 23:41:32 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -98,15 +98,18 @@ if navbar == 'Kalkulator Kalibrasi' :
                 st.write('ADA DATA YANG BELUM TERISI!')
 
     with tab2 :
-        
         st.title('Kesimpulan')
         st.write('Dari perhitungan yang telah didapat, diperoleh kesimpulan sebagai berikut :')
         
         tombol = st.button('Kesimpulan akhir')
         
         if tombol:
+            Nilai_densitas_airZ = 0.999974 - (((Suhu_air - 3.989)**2) * (Suhu_air + 338.636)) / (563385.4 * (Suhu_air + 72.45147))
+            Nilai_densitas_udaraS = ((0.464554 * Tekanan_udara) - (Kelembaban_udara * (0.00252 * Suhu_udara - 0.020582)))/((237.15 + Suhu_udara) * 1000)
+            Nilai_volume_sebenarnyaK = (Massa_air * (1 - Koefisien_muai_volume * (Suhu_air - 20))) / (Densitas_air - Densitas_udara)
             Kesimpulan_akhir = st.write(Nilai_volume_sebenarnyaK)
-            st.success(f'Didapat nilai Volume sebenarnya adalah: ', (Kesimpulan_akhir))                   
+        
+            st.success(f'Didapat nilai Volume sebenarnya adalah {Kesimpulan_akhir}')                   
             
             
 # Home #
